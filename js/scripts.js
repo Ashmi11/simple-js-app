4ca36096432176f0933febaf908f8d8370de17d3
Ashmi11 <ashmita.c@misb.ac.th>
2025-02-16 10:55:07 +0100

    Declared relevant data types
    created a general array and specific objects to be able to store the data related to pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
-alert('Hello world');
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let pokemonList = []; // Declaraing empty array-will be used to store pokemon data
+
+//Adding 3 elements to arraty(pokemons of type object)
+pokemonList[0] = {
+  name: "Bulbasaur",
+  height: 0.7,          // in m
+  types: ['grass', 'poison']  // array of strings
+}
+
+pokemonList[1] = {
+  name: "Charizard",
+  height: 1.7,
+  types: ['fire', 'flying']
+
+}
+
+pokemonList[2] = {
+  name: "Butterfree",
+  height: 1.1,
+  types: ['bug', 'flying']
+
+}
+
+
