cc37a7e489864f00b4f646936e267eec7af0572b
Ashmi11 <ashmita.c@misb.ac.th>
2025-02-23 18:03:28 +0100

    updated for loop to foreach method
    using foreach method to iterate over array

--- js/scripts.js
+++ js/scripts.js
@@ -37,43 +37,13 @@ let pokemonRepository = (function () {
 
 
 
-
-
-
-
-let pokemonList = []; // Declaraing empty array-will be used to store pokemon data
-
-//Adding 3 elements to arraty(pokemons of type object)
-pokemonList[0] = {
-  name: "Bulbasaur",
-  height: 0.7,          // in m
-  types: ['grass', 'poison']  // array of strings
-}
-
-pokemonList[1] = {
-  name: "Charizard",
-  height: 1.7,
-  types: ['fire', 'flying']
-
-}
-
-pokemonList[2] = {
-  name: "Butterfree",
-  height: 1.1,
-  types: ['bug', 'flying']
-
-}
-
-
-//looping over the array with a condition
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1.1) {
-    document.write(pokemonList[i].name + "(height:" + pokemonList[i].height + ")" + " - Wow that's big!<br>");
+//using foreach method for looping over the array with a condition
+pokemonRepository.getAll().forEach(pokemon => {
+  if (pokemon.height > 1.1) {
+    document.write(`${pokemon.name} (height: ${pokemon.height}) - Wow that's big!<br>`);
+  } else {
+    document.write(`${pokemon.name} (height: ${pokemon.height})<br>`);
   }
-  else {
-    document.write(pokemonList[i].name + "(height:" + pokemonList[i].height + ")<br>"); //<br> (line breaks) used to display pokemons on different lines..
-  }
-}
-
+});
 
 
